5a1580c7da0a49e76c7fc0bc3a34d8ae3fe9efc8
Joe Oppegaard <joe.oppegaard@gmail.com>
2017-01-09 03:19:31 +0000

    updates from brett

--- resources/views/unid-academy.blade.php
+++ resources/views/unid-academy.blade.php
@@ -22,8 +22,15 @@
 		<!-- /.row -->
 
 		<!-- Content Row -->
-		<div class="row">
-			<iframe width="100%" height="1200" src="https://sites.google.com/a/hawaii.edu/unidescriptionproject/" frameborder="0"></iframe>
+		<div class="row blade">
+			<div class="container">	
+				<div class="col-md-12">
+                    <img class="photo" src="{{ SITEROOT }}/images/unid-poster.jpg" alt="Brett's UniD research poster">
+                    <p class="caption">UniD research poster presented by Brett Oppegaard at the annual Association for Computer Machinery SIG Design of Communication (SIGDOC), held in Silver Spring, Maryland, in 2016.</p>
+                            
+                    <iframe width="100%" height="1200" src="https://sites.google.com/a/hawaii.edu/unidescriptionproject/" frameborder="0"></iframe>
+                </div>
+            </div>
 		</div>
 		<!-- End the content -->
 
